7b24c07c35f1ecf66bdd471cfe4d12dab62ca12a
Andrei Grajdean <agrajdean@mail.ru>
2024-04-29 09:08:59 +0300

    выполнение усложнённого задания02

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-const num = 266219;
+let num = 266219,
+  sum,
+  degree;
+
+sum = 2 * 6 * 6 * 2 * 1 * 9;
+console.log(sum);
+
+degree = sum ** 3;
+console.log(degree);
+
+console.log(String(degree).slice(0, 2));
